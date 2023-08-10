df054a5ff7f0965a32409a395e1c1c72b97897ed
Daniel Archila <danielarchila1996@hotmail.com>
2023-08-10 11:03:22 -0500

    se agrega el proyecto

---
+++ programabanco.py
@@ -0,0 +1,13 @@
+# ahorros 150          total mas intereses 157.5
+# ahorros 250          total mas intereses 162.5
+
+
+ahorro1 = 150
+ahorro2 = 250
+
+total1 = ahorro1 * 0.05 + 150
+total2 = ahorro2 * 0.05 + 250
+
+print("Las ganancias del primer ahorro son:",total1)
+print("Las ganancias del segundo ahorro son:",total2)
+
